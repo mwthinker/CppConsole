4ad6b49afada35350dd23c0f2fa6a3ac7e12a670
Mwthinker <mwthinker@yahoo.se>
2017-06-17 13:11:45 +0200

    Updated test, can move a rec block around.

--- srcTest/main.cpp
+++ srcTest/main.cpp
@@ -4,9 +4,24 @@
 
 #include <console/console.h>
 
+class Input {
+public:
+	Input() : left_(false), right_(false), up_(false), down_(false) {
+	}
+
+	bool left_;
+	bool right_;
+	bool up_;
+	bool down_;
+};
+
 class TestConsole : public console::Console {
 public:
 	TestConsole() {
+		x_ = 5;
+		y_ = 5;
+		xPos_ = 5;
+		yPos_ = 5;
 	}
 
 protected:
@@ -104,7 +119,41 @@ protected:
 	}
 
 private:
+	double x_, y_;
+	int xPos_, yPos_;
+
+	Input input_;
+
 	void update(double deltaTime) override {
+		double step = 10 * deltaTime;
+
+		if (y_ - 1 > 1 && input_.up_) {
+			y_ -= step;
+		}
+		if (y_ + 1 < 100 && input_.down_) {
+			y_ += step;
+		}
+		if (x_ - 1 > 1 && input_.left_) {
+			x_ -= step;
+		}
+		if (x_ + 1 < 100 && input_.right_) {
+			x_ += step;
+		}
+
+		if ((int) x_ != xPos_) {
+			xPos_ = (int) x_;
+			setCursorPosition(xPos_, yPos_);
+			setTextColor(console::Color::WHITE);
+			setBackgroundColor(console::Color::RED);
+			print(" ");
+		} else if ((int) y_ != yPos_) {
+			yPos_ = (int) y_;
+			setCursorPosition(xPos_, yPos_);
+			setTextColor(console::Color::WHITE);
+			setBackgroundColor(console::Color::RED);
+			print(" ");
+		}
+
 		console::sleep(1.0/60.0);
 	}
 
@@ -116,16 +165,99 @@ private:
 					case console::Key::ESCAPE:
 						quit();
 						break;
+					case console::Key::UP:
+						input_.up_ = true;
+						break;
+					case console::Key::DOWN:
+						input_.down_ = true;
+						break;
+					case console::Key::LEFT:
+						input_.left_ = true;
+						break;
+					case console::Key::RIGHT:
+						input_.right_ = true;
+						break;
+					case console::Key::KEY_0:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(20, 5);
+						print("0");
+						break;
+					case console::Key::KEY_1:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(22, 5);
+						print("1");
+						break;
+					case console::Key::KEY_2:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(24, 5);
+						print("2");
+						break;
+					case console::Key::KEY_3:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(26, 5);
+						print("3");
+						break;
+					case console::Key::KEY_4:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(28, 5);
+						print("4");
+						break;
+					case console::Key::KEY_5:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(30, 5);
+						print("5");
+						break;
+					case console::Key::KEY_6:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(32, 5);
+						print("6");
+						break;
+					case console::Key::KEY_7:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(34, 5);
+						print("7");
+						break;
+					case console::Key::KEY_8:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(36, 5);
+						print("8");
+						break;
+					case console::Key::KEY_9:
+						setTextColor(console::Color::WHITE);
+						setBackgroundColor(console::Color::RED);
+						setCursorPosition(38, 5);
+						print("9");
+						break;
 				}
-				print("KEYDOWN");
+				
 				break;
 			case console::ConsoleEventType::KEYUP:
 				switch (consoleEvent.keyEvent.key) {
 					case console::Key::RETURN:
 						print("RETURN");
 						break;
+					case console::Key::UP:
+						input_.up_ = false;
+						break;
+					case console::Key::DOWN:
+						input_.down_ = false;
+						break;
+					case console::Key::LEFT:
+						input_.left_ = false;
+						break;
+					case console::Key::RIGHT:
+						input_.right_ = false;
+						break;
 				}
-				print("KEYUP");
 				break;
 			case console::ConsoleEventType::CONSOLERESIZE:
 				clear();
